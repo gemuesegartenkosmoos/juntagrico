23ae656e3d5c53fb4cab52cff75538dd9730159b
gemuesegartenkosmoos <131293788+gemuesegartenkosmoos@users.noreply.github.com>
2023-07-02 21:57:58 +0200

    Änderung INFO_EMAIL zu admin@gemuesegartenkosmoos.ch

--- gemuesegartenkosmoos/settings.py
+++ gemuesegartenkosmoos/settings.py
@@ -169,7 +169,7 @@ ORGANISATION_BANK_CONNECTION = {"PC" : "-",
             "ESR" : ""}
 SHARE_PRICE = "50"
 
-INFO_EMAIL = "werner.schuller@gmx.ch"
+INFO_EMAIL = "admin@gemuesegartenkosmoos.ch"
 SERVER_URL = "www.gemuesegartenkosmoos.ch"
 STYLES = { 'static': ['gemuesegartenkosmoos/css/customize.css']}
 
